e98ccf56228f0870b9cce16b9edc5c23e4aa5b8b
Niels Heltner <nhelt15@student.sdu.dk>
2018-10-24 15:18:04 +0200

    added lookup of views

--- app/src/main/java/sems/activityrecognition/MainActivity.java
+++ app/src/main/java/sems/activityrecognition/MainActivity.java
@@ -3,16 +3,33 @@ package sems.activityrecognition;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button sampleBtn;
+    private TextView totalSamplesTxt;
+    private TextView lastSampleTime;
+    private TextView lastSampleValues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lookUpViews();
     }
 
     public void onSampleButtonClick(View view) {
 
     }
+
+    private void lookUpViews() {
+        sampleBtn = findViewById(R.id.sampleBtn);
+        totalSamplesTxt = findViewById(R.id.totalSamplesTxt);
+        lastSampleTime = findViewById(R.id.lastSampleTime);
+        lastSampleValues = findViewById(R.id.lastSampleValues);
+    }
+
 }
